538c026d10d9ad4e4bebe6b2a63ef346410412fd
Andrew Ng <andrew@technative.eu>
2023-11-03 17:05:16 +0100

    Added ECR repo name to outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "ecr_repository_arn" {
   description = "Full ARN of the repository"
   value       = aws_ecr_repository.this.arn
 }
+
+output "ecr_repository_name" {
+  description = "Name of the repository (in the form aws_account_id.dkr.ecr.region.amazonaws.com/repositoryName)"
+  value       = aws_ecr_repository.this.name
+}
